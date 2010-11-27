4bb281b490a2b33e2d63312fb911dc28fde12f35
ramihg <ramihg@gmail.com>
2010-11-27 15:13:28 -0800

    Whose next?

--- Clue.pl
+++ Clue.pl
@@ -2,15 +2,26 @@
 :- dynamic player_active/1.
 
 setup_players([Player|Players]) :- 
-    % Make sure to remove all previous active players
-    retractall(player_active(_)),
+    % Make sure to remove all previous active players before calling this function
     player(Player),
     assert(player_active(Player)),
     setup_players(Players).
 
 setup_players([]).
 
+% Finding the next turn
 
+% This is the case where there are inactive players between
+% CurrentPlayer and NextPlayer
+next_player(CurrentPlayer, NextPlayer) :- 
+    % Get the first relatively left player
+    relatively_left(CurrentPlayer, NextPlayer),
+    % That's active
+    player_active(NextPlayer),
+    % No more
+    !.
+ 
+    
 
 % Player Ordering
 left_of(missScarlet, colonelMustard).
@@ -19,6 +30,12 @@ left_of(mrsWhite, mrGreen).
 left_of(mrGreen, mrsPeacock).
 left_of(mrsPeacock, professorPlum).
 left_of(professorPlum, missScarlet).
+
+relatively_left(LeftPlayer, RightPlayer) :- left_of(LeftPlayer, RightPlayer).
+relatively_left(LeftPlayer, RightPlayer) :-
+    % There is some players that connect LeftPlayer to RightPlayer
+    left_of(LeftPlayer, DirectlyLeft),
+    relatively_left(DirectlyLeft, RightPlayer).
 
 % Players
 player(missScarlet).
