84f5abc9a9559bca6921645090792b57d02af4e2
ramihg <ramihg@gmail.com>
2010-11-27 21:12:01 -0800

    Changes

--- Clue.pl
+++ Clue.pl
@@ -1,8 +1,17 @@
-% Game setup
+% The players that are currently playing
 :- dynamic player_active/1.
+
 :- dynamic card_in_possession/2.
-:- dynamic card_observed/2.
+% The player that the person running this program is playing (i.e. me)
 :- dynamic player_controlled/1.
+:- dynamic last_play_id/1.
+:- dynamic recorded_suggestion/3.
+:- dynamic recorded_player_response/3.
+
+
+/*
+    Game Setup
+*/
 
 setup_players([]).
 setup_players([Player|Players]) :- 
@@ -16,11 +25,38 @@ setup_cards([Card|Cards]) :-
     card(Card),
     player_controlled(Hero),
     assert(card_in_possession(Hero, Card)),
-    assert(card_observed(Hero, Card)),
     setup_cards(Cards).
 
-% Finding the next turn
+/*
+    Main Gameplay
+    This portion has to do with players doing suggestions
+    and rebuttals
+*/
 
+new_suggestion(Player, suggestion(Location, Suspect, Weapon)) :-
+    % Get a new ID for the suggestion
+    new_play_id(Id),
+    % Record it
+    assert(recorded_suggestion(Id, Player, suggestion(Location, Suspect, Weapon))).
+
+% This happens when someone responds to a move the controlling player makes
+player_suggestion_response(SuggestionId, Opponent, DebunkingCard) :-
+    % We know that the opponent possesses this card
+    assert(card_in_possession(Opponent, DebunkingCard)),
+    % Record this response
+    assert(recorded_player_response(SuggestionId, Opponent, DebunkingCard)).
+
+new_play_id(X) :- 
+    last_play_id(LastId),
+    X is LastId + 1,
+    retract(last_play_id(_)),
+    assert(last_play_id(X)).
+
+/*
+    Game Rules
+*/
+
+% Finding the next turn
 next_player(CurrentPlayer, NextPlayer) :- 
     % Get the first relatively left player
     relatively_left(CurrentPlayer, NextPlayer),
